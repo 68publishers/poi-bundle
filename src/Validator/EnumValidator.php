cd2a7966718239dd41130aecdf39210b0750b7f2
tg666 <tomasglawaty@icloud.com>
2020-12-13 02:30:19 +0100

    Added Enum validator

---
+++ src/Validator/EnumValidator.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SixtyEightPublishers\PoiBundle\Validator;
+
+use SixtyEightPublishers\PoiBundle\Exception\AttributeValueException;
+
+final class EnumValidator
+{
+	/** @var array  */
+	private $enum;
+
+	/**
+	 * @param array $enum
+	 */
+	public function __construct(array $enum)
+	{
+		$this->enum = $enum;
+	}
+
+	/**
+	 * @param mixed $value
+	 *
+	 * @return bool
+	 * @throws \SixtyEightPublishers\PoiBundle\Exception\AttributeValueException
+	 */
+	public function __invoke($value): bool
+	{
+		if (!in_array($value, $this->enum, TRUE)) {
+			throw AttributeValueException::validationError(sprintf(
+				'The value must be one of these: [%s]',
+				implode(', ', $this->enum)
+			));
+		}
+
+		return TRUE;
+	}
+}
